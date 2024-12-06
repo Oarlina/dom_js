9dfed012161bddbedc5a6822684e8c9c44cf08e2
Rachel Marquant <marquantrachel@gmail.com>
2024-12-06 17:00:32 +0100

    Ajout de choix pour le nombre et la difficulté

--- js/main.js
+++ js/main.js
@@ -25,43 +25,70 @@ function shuffleChildren(parent)
         parent.appendChild(temp); // place l'élément qui etais a la postion k a la fin
     }
 }
+function showReaction (type, clickedBox){
+    clickedBox.classList.add(type);
+    if (type !== "success")
+    {
+        setTimeout (function(){
+            clickedBox.classList.remove(type);
+        },800);
+    }
+}
 
 
 const box = document.createElement("div"); // la constante box instancie un nouvel objet htmlElement qui est représenter par div. 
 box.classList.add("box"); // on rajoute la class box a classList
 
 const board = document.querySelector("#board"); // on cree la constante board qui recupere la id board
+let nbNombre = prompt("Entrer le nombre maximum à rechercher: ");
+let dificulte = prompt("Voulez-vous la version difficile? (non ou oui)");
+if (nbNombre>1)
+{
 
-let nb = 1;
-for (let i = 1; i <= 10; i++)
-    {
-        const newBox = box.cloneNode();
-        newBox.innerText = i; // rajoute du texte "1" a la constante box
-        board.appendChild(newBox); // rajoute un element a la fin de la constant box
-
-        newBox.addEventListener("click", function(){ // on rajoute l'evenement click sur les boites
-            if (i == nb)
-            {
-                newBox.classList.add("box-valid");
-                console.log(`Boite n° ${i} click! ${nb}`);
-                if (nb == board.children.length)
+    let nb = 1;
+    for (let i = 1; i <= nbNombre; i++)
+        {
+            const newBox = box.cloneNode();
+            newBox.innerText = i; // rajoute du texte "1" a la constante box
+            board.appendChild(newBox); // rajoute un element a la fin de la constant box
+    
+            newBox.addEventListener("click", function() // on rajoute l'evenement click sur les boites
+            { 
+                if (i == nb)
                 {
-                    alert("Victoire !"); // afficher un message en haut de la page 
+                    newBox.classList.add("box-valid");
+                    console.log(`Boite n° ${i} click! ${nb}`);
+                    if (nb == board.children.length)
+                    {
+                        // alert("Victoire !"); // afficher un message en haut de la page 
+                        board.querySelectorAll(".box").forEach(function(box){
+                            showReaction("success",box);
+                        }); // remet toute les cases au depart
+                    }
+                    if (dificulte == "oui")
+                    {
+                        shuffleChildren(board); // pour optimiser le code afin de mélanger les nombres
+                    }
+                    nb++;
+                }else if (i > nb)
+                {
+                    // alert("Erreur, recommencez !"); // afficher un message en haut de la page 
+                    showReaction("error", newBox);
+                    nb = 1;
+                    board.querySelectorAll(".box-valid").forEach(function(validBox){
+                        validBox.classList.remove("box-valid");
+                    }) // remet toute les cases au depart
+                    shuffleChildren(board); // pour optimiser le code afin de mélanger les nombres
+                }else 
+                {
+                    // console.log("Case déjà cliqué !");
+                    showReaction("notice", newBox);
                 }
-                nb++;
-            }else if (i > nb)
-            {
-                alert("Erreur, recommencez !"); // afficher un message en haut de la page 
-                nb = 1;
-                board.querySelectorAll(".box-valid").forEach (function(validBox){
-                    validBox.classList.remove("box-valid");
-                })
-            }else 
-            {
-                console.log("Case déjà cliqué !");
-            }
-        })
+            })
+    }
+    
+    shuffleChildren(board); // pour optimiser le code afin de mélanger les nombres
+}else{
+    nbTest = prompt("Entrer le nombre maximum à rechercher(minimum 2): ");
 }
-
-shuffleChildren(board); // pour optimiser le code
 
